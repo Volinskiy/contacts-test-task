96d23ff1c96a2f589a7492b402aa49c3e2eeeb91
Sergey <you@example.com>
2021-09-13 17:01:58 +0300

    1) Configuration prettier and elsont 2) Create page Contacts, add request data

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,30 @@
+import { useState, useEffect } from 'react'
+
 export function Contacts() {
-    <h1>Hello</h1>
+	const [contacts, setContacts] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
+	const [isError, setIsError] = useState(false)
+
+	useEffect(() => {
+        setIsLoading(true)
+		fetch('https://randomuser.me/api/?esults=200')
+			.then((response) => response.json())
+			.then((data) => {
+				setContacts(data.results)
+				setIsLoading(false)
+			})
+			.catch(() => {
+				setIsLoading(false)
+				setIsError(true)
+			})
+        }, [])
+
+	if (isLoading) {
+		return <div>...loading</div>
+	}
+
+	if (isError) {
+		return <div>...error</div>
+	}
+	return <h1>Hello</h1>
 }
